9493a4bc6f9659f847718f2c66af992f8c0a1cdd
Julio Nunes Avelar <jn5.guape@gmail.com>
2024-03-22 20:02:03 -0300

    Rearoveitando partes do Risco 5
    Importando testes, sistemas de debug e testes de software do Risco 5

---
+++ tests/soc_test.v
@@ -0,0 +1,37 @@
+module soc_tb();
+
+reg clk, reset;
+wire rx, tx;
+wire [7:0] led;
+
+always #1 clk = ~clk;
+
+Risco_5_SOC #(
+    .CLOCK_FREQ(25000000),
+    .BIT_RATE(115200),
+    .MEMORY_SIZE(4096),
+    .MEMORY_FILE("software/memory/generic.hex")
+) SOC(
+    .clk(clk),
+    .reset(reset),
+    .leds(led),
+    .rx(rx),
+    .tx(tx)
+);
+
+initial begin
+    $dumpfile("build/soc.vcd");
+    $dumpvars;
+
+    clk = 1'b0;
+    reset = 1'b1;
+    #6
+    reset = 1'b0;
+    //#560
+
+    #1200
+
+    $finish;
+end
+
+endmodule

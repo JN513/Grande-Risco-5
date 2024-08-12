166fd078e55ef1e0209ae76cb87cc69ae992f6cb
Julio Nunes Avelar <jn5.guape@gmail.com>
2024-08-12 10:29:00 -0300

    Criando novos testes para testar o pipeline

--- run_test.sh
+++ run_test.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/zsh
 
+mkdir -p build
+
 echo "Digite o nome do teste:"
 read nome_do_teste
 
 cp software/memory/$nome_do_teste.hex software/memory/generic.hex
 
-iverilog -o build/soc_test.o -s soc_tb src/core/* src/peripheral/*.v tests/soc_test.v
-vvp build/soc_test.o
+iverilog -o build/core_test.o -s core_tb src/core/* src/peripheral/memory.v tests/core_test.v
+vvp build/core_test.o
 
 rm software/memory/generic.hex
 
